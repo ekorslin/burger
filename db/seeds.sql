f68d3ee46c49e08fb06596dedcd99bbe14b3b3c8
ekorslin <eric.korslin@gmail.com>
2018-09-01 07:08:22 -0500

    firstCommit

---
+++ db/seeds.sql
@@ -0,0 +1,16 @@
+/* Seeds for SQL table. We haven't discussed this type of file yet */
+USE burgers_db;
+
+/* Insert 3 Rows into your new table */
+-- INSERT INTO burgers (burger_name)
+-- VALUES ("Bacon Chesseburger");
+
+-- INSERT INTO burgers (burger_name)
+-- VALUES ("Big Mac");
+
+-- INSERT INTO colleges (name)
+-- VALUES ("Harvard");
+
+/* OR */
+INSERT INTO burgers (burger_name)
+VALUES ("Big Mac"), ("Bacon Cheeseburger"), ("The Krusty Burger");
